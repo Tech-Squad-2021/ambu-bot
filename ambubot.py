f238f8cda561437eb9869f0dfc14e467d2a96a73
Ronit Banerjee <91361382+ronitblenz@users.noreply.github.com>
2022-03-02 01:54:01 +0530

    updates 1.0

--- ambubot.py
+++ ambubot.py
@@ -1,3 +1,31 @@
+from flask import Flask, request
+from twilio.rest import Client
+import scrapper
+import json
+# import os
+
+# from dotenv import load_dotenv
+# load_dotenv()
+
+# account_sid = os.environ['TWILIO_ACCOUNT_SID']
+# auth_token = os.environ['TWILIO_AUTH_TOKEN']
+# client = Client(account_sid, auth_token)
+
+from config import TWILIO_AUTH_TOKEN, TWILIO_ACCOUNT_SID
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+
+CITIES = json.load(open('./cities.json'))
+ambulance = "\U0001F691"
+medical = "\U00002695"
+
+intro = """
+Hey! I'm AmbuBot! {} {}
+
+Before that, please send me you current location.
+
+e.g. type "Kolkata" and Press the Send Button
+""".format(ambulance, medical)
 def send_msg(sender_number, reciever_number, message):
     client.messages.create(
         to=reciever_number,
